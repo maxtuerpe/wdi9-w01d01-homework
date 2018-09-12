f3806bc2bc4525b55a89c7d3e2199cfad23510db
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-12 07:06:21 -0600

    homeowrk done

---
+++ homework-part-2/js/app.js
@@ -0,0 +1,84 @@
+// // concatenation is when multiple strings are added together 
+// console.log('hello,'+' how are you today?');
+// // interpolation is almost the same exept you can use variables
+// const color = "blue";
+// console.log('that is a' + color + 'marker');
+
+// DRY stands for "Don't repeat yourself". 
+// When we are doing the same comand over and over again we can use loops to minimize the code.
+
+// declaring a variable is when you creat a new variable
+// defining a variable is giving it a value
+// using const gives you a variable that you can't change the vale of latter on in the code
+// using let allows the variable to be changed
+// a parent directory containes other directories
+// using the man command before another command pulls up more information about it in the terminal
+// tab completion autocompletes names of something you have typed the beging of. you don't have to type out everything always
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+// console.log(a < b);
+// console.log(c > d);
+// console.log(a < b);
+// console.log( 'name' === 'name');
+// console.log(a < b < c);
+// console.log(a === a < d);
+// console.log(a < b < c);
+// console.log(e === "Kevin");
+// console.log(48 == '48');
+
+
+// while (true){
+//     console.log('do not run this loop');
+// }
+// //this is an infinite loop. true is always true
+// const runProgram = true;
+
+// while (runProgram){
+//     console.log ('do not run this program');
+//     runProgram = false;
+// }
+// //this loop won't run because we are trying to reassign a constant varianle.
+
+// // sets up the variable letters as a string containing "A".
+// let letters = 'A'
+// // sets up the variable i as the number 0.
+// let i = 0;
+// // this while loop will run until i reaches 20
+//  while (i< 20){
+//      // adds the character "A" the the string
+//      letters += 'A';
+//      // increases i by 1
+//      i++;
+//  }
+//  // logs letters after the loop has run.
+//  console.log(letters);
+//  // I expect an output of 'AAAAAAAAAAAAAAAAA'.
+//  // yup.
+
+// // while loops run for the whole time untill thier expression is false
+// // for loops set up a number of times to run
+// for(i = 0; i < 1000; i++){
+//     console.log(i);
+// }
+// //the first part of the statement defines the iterater
+// // the second part says what value to run to
+// // the third part changes the value of the iterator
+// for(i = 999; i >= 0; i--){
+//     console.log(i);
+// }
+for(i=0; i< 10; i++){
+    console.log('The value of i is: ' + i + ' of 10');
+}
+
+
+
+
+
+
+
+
+
+
